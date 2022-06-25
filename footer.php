3e97cb7545380951ce0e116fb586a5c9ae2cf423
biorabiei <91165416+biorabiei@users.noreply.github.com>
2022-06-26 04:22:28 +0430

    Initial commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Twing
+ */
+
+?>
+
+
+<?php wp_footer(); ?>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
+</body>
+</html>
